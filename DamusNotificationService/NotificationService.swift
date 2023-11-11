32f2e92fd60842747b15bd42bad871c9ada3c674
Daniel D’Aquino <daniel@daquino.me>
2023-11-10 16:12:54 -0800

    Extract nostr event data from notification

--- DamusNotificationService/NotificationService.swift
+++ DamusNotificationService/NotificationService.swift
@@ -6,6 +6,32 @@
 //
 
 import UserNotifications
+import damus
+
+/// The representation of a JSON-encoded Nostr Event used by the push notification server
+/// Needs to match with https://gitlab.com/soapbox-pub/strfry-policies/-/raw/433459d8084d1f2d6500fdf916f22caa3b4d7be5/src/types.ts
+struct NostrEventInfoFromPushNotification: Codable {
+    let id: String          // Hex-encoded
+    let sig: String         // Hex-encoded
+    let kind: Int
+    let tags: [[String]]
+    let pubkey: String      // Hex-encoded
+    let content: String
+    let created_at: Int
+    
+    static func from(dictionary: [AnyHashable: Any]) -> NostrEventInfoFromPushNotification? {
+        guard let id = dictionary["id"] as? String,
+              let sig = dictionary["sig"] as? String,
+              let kind = dictionary["kind"] as? Int,
+              let tags = dictionary["tags"] as? [[String]],
+              let pubkey = dictionary["pubkey"] as? String,
+              let content = dictionary["content"] as? String,
+              let created_at = dictionary["created_at"] as? Int else {
+            return nil
+        }
+        return NostrEventInfoFromPushNotification(id: id, sig: sig, kind: kind, tags: tags, pubkey: pubkey, content: content, created_at: created_at)
+    }
+}
 
 class NotificationService: UNNotificationServiceExtension {
 
@@ -14,11 +40,24 @@ class NotificationService: UNNotificationServiceExtension {
 
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
         
         if let bestAttemptContent = bestAttemptContent {
             // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
+            guard let nostrEventInfoDictionary = request.content.userInfo["nostr_event"] as? [AnyHashable: Any],
+                  let nostrEventInfo = NostrEventInfoFromPushNotification.from(dictionary: nostrEventInfoDictionary) else {
+                contentHandler(request.content)
+                return;
+            }
+            
+//            guard let ndb = Ndb() else {
+//                contentHandler(request.content)
+//            }
+//            
+//            ndb.process_event(JSONEncoder().encode(nostrEventInfo))
+//            
+//            let ndbTxn: NdbTxn<NostrEvent?> = ndb.lookup_note(nostrEventInfo.id)
+//            
+//            bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
             
             contentHandler(bestAttemptContent)
         }
